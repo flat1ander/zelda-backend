cb5d4a948ddd2b2082ec8d94c58a53044de85ad6
flat1ander <thematthewlane@gmail.com>
2023-05-25 20:12:30 -0400

    Adjusted index route to include mongo IDs now.

--- Routers/monstersRouter.js
+++ Routers/monstersRouter.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const monsters = require('../models/monsterData.json');
 
 
-// Index route at /books
-router.get('/', (req, res) => {
-    res.json(monsters);
+// Index route at /monsters
+router.get('/', async (req, res) => {
+    res.json(await Monster.find({})).status(200);
 })
 
 // Seed Route:
@@ -18,12 +18,12 @@ router.get('/seed', async (req, res) => {
 
 // Show Route:
 router.get('/:id', async (req, res) => {
-    res.json(await Monster.findById(req.params.id))
+    res.json(await monsters.findById(req.params.id))
 })
 
 // Post Route:
 router.post ('/', async (req, res) => {
-    res.json(await Monster.create(req.body))
+    res.json(await monsters.create(req.body))
 })
 
 // Put Route:
